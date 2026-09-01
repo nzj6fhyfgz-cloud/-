231feaad7ce916e3968ef6619a59bab7ee46db51
nzj6fhyfgz-cloud <nzj6fhyfgz@privaterelay.appleid.com>
2026-09-01 14:44:21 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="st-kokushi-v1";
+const CACHE="st-kokushi-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
